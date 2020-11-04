1ec0c66609c1775ad164aa150c601bacd94b8b1c
Herman Westerman <58404234+namwe@users.noreply.github.com>
2020-11-04 12:21:23 +0100

    Test add

---
+++ Test.cpp
@@ -0,0 +1,5 @@
+//
+// Created by herman on 11/4/20.
+//
+
+#include "Test.h"
---
+++ Test.h
@@ -0,0 +1,14 @@
+//
+// Created by herman on 11/4/20.
+//
+
+#ifndef CPROG_PROJEKT_TEST_H
+#define CPROG_PROJEKT_TEST_H
+
+
+class Test {
+
+};
+
+
+#endif //CPROG_PROJEKT_TEST_H
