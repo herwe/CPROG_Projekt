5d1370ea42c59225ce4e505d97680b7b2df2cd88
Herman Westerman <58404234+namwe@users.noreply.github.com>
2020-11-27 13:55:24 +0100

    Change game type

--- main.cpp
+++ main.cpp
@@ -6,9 +6,9 @@
 
 
 int main() {
-    SDL_Window* window = SDL_CreateWindow("Tower of Hanoi", SDL_WINDOWPOS_UNDEFINED,
+    SDL_Window* window = SDL_CreateWindow("Space Invaders", SDL_WINDOWPOS_UNDEFINED,
                                           SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
-    SDL_Surface* image = SDL_LoadBMP("/home/herman/Documents/hanoi.bmp");
+    SDL_Surface* image = SDL_LoadBMP("/home/herman/Documents/space.bmp");
     SDL_Renderer* render = SDL_CreateRenderer(window, -1, 0);
     SDL_Texture* texture1 = SDL_CreateTextureFromSurface(render, image);
 
